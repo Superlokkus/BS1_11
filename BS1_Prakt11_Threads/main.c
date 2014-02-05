eea9c1714c45d3c6e0266a001f7003befd562336
Markus Klemm <markus@markusklemm.net>
2014-02-05 13:13:34 +0100

    Initial Commit

---
+++ BS1_Prakt11_Threads/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  BS1_Prakt11_Threads
+//
+//  Created by Markus Klemm on 05.02.14.
+//  Copyright (c) 2014 net.markusklemm. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
