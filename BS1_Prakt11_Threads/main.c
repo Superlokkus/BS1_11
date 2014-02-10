1c376bcdd097c224aaa865a09608a3cf22e30f12
Markus Klemm <markus@markusklemm.net>
2014-02-10 01:15:59 +0100

    Very time consuming

--- BS1_Prakt11_Threads/main.c
+++ BS1_Prakt11_Threads/main.c
@@ -11,13 +11,21 @@
 #include <string.h>
 #include <stdlib.h>
 
-long lnumber;
+long lnumber; pthread_mutex_t changeLnumberMutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* hundredMillionDecrement(void *redherring)
 {
     int_least32_t i;
     for (i=0; i<100000000; i++) {
+        if (pthread_mutex_lock(&changeLnumberMutex)) {
+            perror("Locking mutex in 2nd thread failed");
+            return NULL;
+        }
         lnumber--;
+        if (pthread_mutex_unlock(&changeLnumberMutex)) {
+            perror("Unlocking mutex in 2nd thread failed");
+            return NULL;
+        }
     }
     return NULL;
 }
@@ -25,6 +33,8 @@ void* hundredMillionDecrement(void *redherring)
 int main(int argc, const char * argv[])
 {
     printf("Global number initialized with %ld\n",lnumber);
+    
+    
     
     pthread_t secondThread;
     if (pthread_create(&secondThread, NULL, hundredMillionDecrement, NULL)) {
@@ -34,14 +44,31 @@ int main(int argc, const char * argv[])
     
     int_least32_t i;
     for (i=0; i<100000000; i++) {
+        if (pthread_mutex_lock(&changeLnumberMutex))
+        {
+            perror("Locking the mutex failed in main thread");
+            return EXIT_FAILURE;
+        }
         lnumber++;
+        if (pthread_mutex_unlock(&changeLnumberMutex)) {
+            perror("Unlocking the mutex failed in main thread");
+            return EXIT_FAILURE;
+        }
     }
     
     if (pthread_join(secondThread, NULL)) {
         perror("Joining 2nd thread failed");
+        return EXIT_FAILURE;
     }
     
-    printf("Main exiting, global number is %ld\n",lnumber);
+    printf("Result of global number: %ld\n",lnumber);
+    
+    //Cleanup
+    if (pthread_mutex_destroy(&changeLnumberMutex))
+    {
+        perror("Destroying mutex failed");
+        return EXIT_FAILURE;
+    }
     
     return EXIT_SUCCESS;
 }
